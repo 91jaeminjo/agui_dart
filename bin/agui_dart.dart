23b615fd33f8f0437c1a2f72b335b8b22ccfb9ee
Jaemin Jo <44039707+91jaeminjo@users.noreply.github.com>
2025-10-23 18:31:38 -0500

    initial commit

---
+++ bin/agui_dart.dart
@@ -0,0 +1,5 @@
+import 'package:agui_dart/agui_dart.dart' as agui_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${agui_dart.calculate()}!');
+}
---
+++ test/agui_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:agui_dart/agui_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
